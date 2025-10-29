1474d6519a0a0a8a378a6f2713ada1c5f2814a47
rajkumar <madheshiya.rajkumar07@gmail.com>
2025-10-29 07:40:17 +0530

    Updating saas application with celark authentication

--- saas/api/index.py
+++ saas/api/index.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI  # type: ignore
+import os
+from fastapi import FastAPI, Depends  # type: ignore
 from fastapi.responses import StreamingResponse  # type: ignore
+from fastapi_clerk_auth import ClerkConfig, ClerkHTTPBearer, HTTPAuthorizationCredentials  # type: ignore
 from openai import OpenAI  # type: ignore
 
 app = FastAPI()
 
+jwksurl = os.getenv("CLERK_JWKS_URL")
+print("Environment CLERK_JWKS_URL:", jwksurl) 
+if not jwksurl:  
+    jwksurl = "https://clerk.com/.well-known/jwks.json"
+
+clerk_config = ClerkConfig( jwks_url=jwksurl )
+clerk_gaurd = ClerkHTTPBearer(clerk_config)
+
 @app.get("/api")
-def idea():
+def idea( creds: HTTPAuthorizationCredentials = Depends(clerk_gaurd)):
+    user_id = creds.decoded["sub"] # User ID from Clerk token- can be used for further user-specific logic
     client = OpenAI()
     prompt = [{"role": "user", "content": "Reply with a new business idea for AI Agents, formatted with headings, sub-headings and bullet points"}]
     stream = client.chat.completions.create(model="gpt-5-nano", messages=prompt, stream=True)
--- saas/pages/_app.tsx
+++ saas/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { ClerkProvider } from '@clerk/nextjs';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} />
+    </ClerkProvider>
+    );
 }
